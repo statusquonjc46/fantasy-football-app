2c8882aa9a1a026dd005d381ad4c9845b8eb147c
statusquonjc46 <nicholas.coletta@gmail.com>
2025-06-01 21:25:19 -0400

    added api package, database package, api call is working, data analysis started.

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/statusquonjc46/fantasy-football-app/internal/api/api.go"
-	"io"
-	"net/http"
-	"os"
+	"github.com/statusquonjc46/fantasy-football-app/internal/api"
 )
 
 type returnError struct {
@@ -31,68 +27,7 @@ func main() {
 	fmt.Println("Enter the espn_s2 Cookie value:")
 	fmt.Scanln(&espnS2)
 
-	league := &api.leagueInfo{
-		LeagueID: leagueID,
-		Year:     year,
-		V2Api:    "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/leagueHistory/{leagueid}?seasonId={year}",
-		V3Api:    "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{leagueid}",
-		SWID:     swid,
-		S2:       espnS2,
-	}
+	callData := api.ProcessUserInput(leagueID, year, swid, espnS2)
+	api.MakeCall(callData)
 
-	url, err := league.formatApiCall()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	client := &http.Client{}
-
-	// Create a new HTTP GET request
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
-	}
-
-	// Add a cookie to the request
-	cookieSWID := &http.Cookie{
-		Name:  "swid",
-		Value: league.SWID,
-	}
-	req.AddCookie(cookieSWID)
-
-	cookieS2 := &http.Cookie{
-		Name:  "espn_s2",
-		Value: league.S2,
-	}
-	req.AddCookie(cookieS2)
-
-	// Send the request
-	resp, err := client.Do(req)
-	if err != nil {
-		err := fmt.Errorf("[Error] - Failed to make request to ESPN api: %w\n", err)
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		err := fmt.Errorf("[Error] - Request failed with status code: %d\n", resp.StatusCode)
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		err := fmt.Errorf("[Error] - Failed to read response body: %w", err)
-		fmt.Println(err)
-	}
-
-	var printJson string
-	err = json.Unmarshal(body, &printJson)
-	if err != nil {
-		err := fmt.Errorf("[Error] - Error unmarshalling JSON: %w", err)
-		fmt.Println(err)
-	}
-
-	fmt.Println(printJson)
 }
